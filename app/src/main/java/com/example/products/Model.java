741cc23bd547c238c4943e7ea3d382ade7f39045
Savinu <it20270674@my.sliit.lk>
2021-09-26 14:57:21 +0530

    savinu commit

---
+++ app/src/main/java/com/example/products/Model.java
@@ -0,0 +1,54 @@
+package com.example.products;
+
+public class Model {
+    String id, product , price , quantity , date;
+    public Model(){}
+
+    public Model(String id, String product, String price , String quantity , String date){
+        this.id = id;
+        this.product = product;
+        this.price = price;
+        this.quantity = quantity;
+        this.date = date;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
