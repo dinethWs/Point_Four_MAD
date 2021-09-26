9f61e0fc74e9d69c994c7931d0bca103713fa274
Nisura-00 <it20133504@my.sliit.lk>
2021-09-26 09:47:38 +0530

    ball

---
+++ app/src/main/java/com/example/products/HomeDisplay.java
@@ -0,0 +1,14 @@
+package com.example.products;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_display);
+    }
+}
